4b1c342a492488aa3303b14f772344d3120cef8f
Al-Mamun Sarkar <msarkar.cse@gmail.com>
2025-03-12 13:43:21 +0600

    #test
    Prompt Category:
    Unit Test Writing
    
    Prompt:
    How can I run both test together?
    
    User Observation:
    It's created test runner to run all test cases together.
    
    Response ID:
    6abadbbd-fda4-41c4-be1c-bf8ded9c3620

---
+++ run_tests.py
@@ -0,0 +1,8 @@
+"""Test runner module."""
+import unittest
+
+if __name__ == '__main__':
+    test_loader = unittest.TestLoader()
+    test_suite = test_loader.discover('tests', pattern='test_*.py')
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(test_suite)
